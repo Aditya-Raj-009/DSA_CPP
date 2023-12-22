02af938bc9bd704a9da827eaa20218cfcddffad1
Aditya-Raj-009 <adityasinghrajput2107200@gmail.com>
2023-12-22 20:20:14 +0530

    initial commit

---
+++ Arrays/rotateArray.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int  GCD(int a ,int b)
+{
+    if(a == 0)
+    return b;
+    return GCD(b%a,a);
+}
+void rotateArray(int arr[] , int  n, int kTimes)
+{
+    kTimes%=n; // kTimes >=n
+
+    int gcd = GCD(kTimes,n);
+
+    for(int i = 0; i<gcd; i++)
+    {
+        int j = i;
+        int temp = arr[i];
+
+        while(1)
+        {
+            int r = j+kTimes;
+            if(r>=n)
+            {
+                r-=n;
+            }
+            if (r == i)
+            {
+                break;
+            }
+            arr[j] = arr[r];
+            j = r;
+        }
+        arr[j] = temp;
+    }
+}
+
+
+int main()
+{
+    int arr [] = {1,2,3,4,5,6};
+    rotateArray(arr,sizeof(arr)/sizeof(int),2);
+
+    for(int e:arr)
+    {
+        cout<<e<<" ";
+    }
+}
